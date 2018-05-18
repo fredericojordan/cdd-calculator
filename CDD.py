35d0c75e36d05bd634ab61a5160e21660e44b703
Marcelo <marcelosalles@github.com>
2018-05-18 14:26:47 -0300

    adding comments to code and requirements.txt

--- CDD.py
+++ CDD.py
@@ -1,54 +1,77 @@
 '''
-Calcula CDD das temperaturas definidas pelo intervalo definido.
+Calculates CDD for different temperature thresholds defined
 '''
 
 import glob
 import pandas
 
+# Creates a list with the degrees chosen to be the threshold.
+# If it's not needed to pick one by one, you can create a list called "THRESHOLDS"
+# with whatever values you want.
 MIN_THRESHOLD = 15
 MAX_THRESHOLD = 22
-
-FILES = glob.glob('*.epw')
-print(FILES)
-
 THRESHOLDS = list(range(MIN_THRESHOLD, MAX_THRESHOLD+1))
 
+# Lists weather files in folder
+FILES = glob.glob('*.epw')
+#print(FILES)
+
 def cdd_add(threshold, t):
+	# Calculates how much temperature is above threshold
+
 	if t > threshold:
 		cdd = t - threshold
 	else:
 		cdd = 0
 	return(cdd)
 
-DATA = {'Climate': []} #, 'CDD_DRY': [],'CDD_WET': []}
+# Creates a dictionary to organize the values
+DATA = {'FILE_NAME': []}
 
+# Creates a CDD for each threshold defined by the THRESHOLD list
 for threshold in THRESHOLDS:
 	DATA['CDD'+str(threshold)+'_DRY'] = []
 	DATA['CDD'+str(threshold)+'_WET'] = []
 
+# Reads each .epw file listed in the folder
 for file in FILES:
 
-	cdd_dry = [0] * (MAX_THRESHOLD+1 - MIN_THRESHOLD)
-	cdd_wet = [0] * (MAX_THRESHOLD+1 - MIN_THRESHOLD)
+	# Creates a list where each value on the list will be the CDD for each threshold
+	cdd_dry = [0] * (len(THRESHOLDS))
+	cdd_wet = [0] * (len(THRESHOLDS))
 
+	# Opens each file
 	with open(file, encoding = 'latin-1') as file_reader:
+		
+		# Condition created so it doesn't take values from the header
 		start_sum = False
+		
+		# Reads each line of the .epw file
 		for line in file_reader:
+			
+			# Checks if it's over reading the header
 			if start_sum:
+
+				# Splits lines by comma
 				line = line.split(',')
+
+				# adds how much line's temperature is above threshold and adds it to the sum
 				for i in range(len(THRESHOLDS)):
 					cdd_dry[i] += cdd_add(THRESHOLDS[i], float(line[6]))
 					cdd_wet[i] += cdd_add(THRESHOLDS[i], float(line[7]))
 
+			# Checks if it's the last line of the header
 			if 'DATA PERIODS' in line:
 				start_sum = True
 
-	DATA['Climate'].append(file)
+	# Adds the filename and the CDDs to the dictionary
+	DATA['FILE_NAME'].append(file)
 	for i in range(len(THRESHOLDS)):			
 		DATA['CDD'+str(THRESHOLDS[i])+'_DRY'].append(cdd_dry[i])
 		DATA['CDD'+str(THRESHOLDS[i])+'_WET'].append(cdd_wet[i])
 
-
+# Turns de DATA as a pandas' Data Frame
 output = pandas.DataFrame(DATA)
 
+# Save pandas' Data Frame to a .csv file
 output.to_csv("CDD.csv", index=False)
